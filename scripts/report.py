f6c58b084df1a186e3bc34e3639f932155ef0680
Rafael Godoy <Rafael.g26@hotmail.com>
2024-06-01 10:46:42 -0300

    adição da distribuição da validação no relatório

--- scripts/report.py
+++ scripts/report.py
@@ -1,8 +1,8 @@
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 import pickle
 from fpdf import FPDF
-from xgb_pipeline import XGBPipeline
 
 class PDF(FPDF):
     def header(self):
@@ -62,7 +62,62 @@ class PDF(FPDF):
         self.chapter_title(model_name)
         self.chapter_body(model_evaluation)
 
+    def add_chart(self, model1, model2, model1_name, model2_name):
+        self.add_page()
+        self.chapter_title(f'{model1_name} x {model2_name}')
+        
+        differences = np.array(model2)-np.array(model1)
+        rho = 1 / 10  # 1/10 pq 10-fold?
+        rope = 0.01
+
+        posterior = bayesian_correlated_t_test(differences, rho)
+
+        x = np.linspace(posterior.ppf(0.001), posterior.ppf(0.999), 1000)
+        y = posterior.pdf(x)
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(x, y, label='Posterior Distribution')
+        plt.fill_between(x, y, color='lightblue', alpha=0.8)
+        plt.axvline(-rope, color='orange', linestyle='--')
+        plt.axvline(rope, color='orange', linestyle='--')
+
+        plt.title('Posterior Distribution of Mean Difference of the Metric (personalized)')
+        plt.xlabel('Difference of Metric (personalized)')
+        plt.ylabel('Probability Density')
+        plt.legend()
+        plt.grid(True)
+
+        image_path = f'./dataset/images/{model1_name}_x_{model2_name}.png'
+        plt.savefig(image_path)
+        plt.close()
+
+        pdf.image(image_path, x=20, y=50, w=180)
+
+        
+        prob_better = 1 - posterior.cdf(0)
+
+        # Probabilidade de equivalência prática (usando uma ROPE de 1%)
+        prob_equiv = posterior.cdf(rope) - posterior.cdf(-rope)
+
+        self.set_font('Times', '', 12)
+
+        self.multi_cell(0, 5, f"Probability of practical equivalence: {prob_equiv:.4f}")
+        self.ln()   
+
+        self.multi_cell(0, 5, f"Probability of {model1_name} being better than {model2_name}: {prob_better:.4f}")
+        self.ln()
+
+def bayesian_correlated_t_test(differences, rho):
+    n = len(differences)
+    mean_diff = np.mean(differences)
+    std_diff = np.std(differences, ddof=1)
     
+    # t distribution parameters
+    dof = n - 1
+    scale = std_diff / np.sqrt(n * (1 + (n - 1) * rho))
+    
+    # Retorns t distribution
+    return stats.t(dof, loc=mean_diff, scale=scale)   
 
 if __name__ == "__main__":
 
@@ -72,11 +127,21 @@ if __name__ == "__main__":
     with open(file_path, 'rb') as file:
         xgb = pickle.load(file)
 
+    file_path = './models/lstm_pipeline_2024-05-31_01_32_33.pkl'
+
+    with open(file_path, 'rb') as file:
+        lstm = pickle.load(file)
+
+
     pdf = PDF()
 
     title = 'Validation Report'
     pdf.set_title('Validation Report')
 
     pdf.print_chapter(xgb, 'xgb_pipeline_2024-05-30_15_37_22')
+
+    pdf.print_chapter(lstm, 'lstm_pipeline_2024-05-31_01_32_33')
+
+    pdf.add_chart(lstm, xgb, 'lstm_pipeline_2024-05-31_01_32_33', 'xgb_pipeline_2024-05-30_15_37_22')
 
     pdf.output('Validation_Report.pdf', 'F')
